4b1078436ec026e93a89ff76a388dd9efdc36992
Kabelin <37315628+Kabelin@users.noreply.github.com>
2025-03-29 06:50:04 -0300

    :bug: remove translations and fix css imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,5 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 
 import "@/infrastructure/ui/styles/reset.css";
-import "@/infrastructure/ui/styles/tailwind.css";
-import "@/infrastructure/ui/styles/tailwind.arche.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
