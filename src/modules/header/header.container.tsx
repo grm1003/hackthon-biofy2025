0e2c4eacc767bd8b38d9181d8f20d29f88e89191
Gabriel Rezende Machado <47564540+grm1003@users.noreply.github.com>
2025-03-29 21:01:07 -0300

    starting

--- src/modules/header/header.container.tsx
+++ src/modules/header/header.container.tsx
@@ -1,15 +1,13 @@
 export const HeaderContainer = () => {
   return (
-    <div className="h-12 w-full border-b border-0 border-gray-200 border-solid flex items-center justify-between px-5">
+    <div className=" dark:bg-black h-12 w-full border-b border-0 border-gray-200 border-solid flex items-center justify-between px-5">
       <div>
-        <span className="font-body lg:text-body-large md:text-body-medium sm:text-body-small">
-          SOS.AI
+    
+        <span className="font-body text-white lg:text-body-large md:text-body-medium sm:text-body-small">
+          ASSURANCE.IA
         </span>
       </div>
       <div className="flex gap-3">
-        {/* <a href="#about">About</a> */}
-        {/* <a href="#about">About</a> */}
-        {/* <a href="#about">About</a> */}
       </div>
     </div>
   );
